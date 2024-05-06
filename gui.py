ddece05158e9001369ed26a7b02ddb6d8e6f50f9
Hoher2000 <costet86@mail.ru>
2024-05-06 23:15:47 +0300

    added drawline method in window class

--- gui.py
+++ gui.py
@@ -2,12 +2,10 @@ from tkinter import Tk, BOTH, Canvas
 
 class Window:
     def __init__(self, width, height) -> None:
-        self.width = width
-        self.height = height
         self.__root = Tk()
         self.__root.title('Maze solver')
-        self.__canvas = Canvas(self.__root)
-        self.__canvas.pack()
+        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
+        self.__canvas.pack(fill=BOTH, expand=1)
         self.__isrun = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
@@ -19,6 +17,26 @@ class Window:
         self.__isrun = True
         while self.__isrun:
             self.redraw()
+        print('window closed')
 
     def close(self):
         self.__isrun = False
+
+    def draw_line(self, line, fill_color):
+        line.draw(self.__canvas, fill_color)
+
+class Point:
+    def __init__(self, x, y) -> None:
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, dot1, dot2) -> None:
+        self.dot1 = dot1
+        self.dot2 = dot2
+
+    def draw(self, Canvas, fill_color):
+        Canvas.create_line(
+            self.dot1.x, self.dot1.y, self.dot2.x, self.dot2.y,
+            fill=fill_color, width=2
+        )
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from gui import Window
+from gui import *
 
 def main():
     win = Window(800, 600)
+    dot1, dot2, dot3 = Point(10, 100), Point(400, 400), Point(100, 100)
+    win.draw_line(Line(dot1, dot2), 'red')
+    win.draw_line(Line(dot3, dot2), 'blue')
     win.wait_for_close()
-
+    
 if __name__ == '__main__':
     main()
